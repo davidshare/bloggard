0d8f0b7ca0298cd1a08c21dc0143c8984e3276f7
David Essien <davidessienshare@gmail.com>
2022-06-17 06:11:40 +0100

    improve description in menu locations

--- wp-content/themes/bloggard/lib/navigation.php
+++ wp-content/themes/bloggard/lib/navigation.php
@@ -1,8 +1,8 @@
 <?php
   function _themename_register_menus(){
     register_nav_menus( array(
-      'main-menu' => esc_html__('Main menu', '_themename'),
-      'footer-menu' => esc_html__('Footer menu', '_themename')
+      'main-menu' => esc_html__('Main menu - Display this menu in the top nav', '_themename'),
+      'footer-menu' => esc_html__('Footer menu - Display this menu in the footer', '_themename')
     ));
   }
 
