071593d27fb79b8c33bad2aa6b44022b9fc34ff1
David Essien <davidessienshare@gmail.com>
2022-06-20 06:40:14 +0100

    add the footer.php file content

--- wp-content/themes/bloggard/footer.php
+++ wp-content/themes/bloggard/footer.php
@@ -1,2 +1,7 @@
-</body>
-</html> 
+  
+  <footer class="footer" role="footer" id="footer">
+    <?php get_template_part('template-parts/footer/widgets') ?>
+  </footer>
+  <?php wp_footer(); ?>
+  </body>
+</html>
