2bd990427e6ecbd59b32a115b56838bdd080ed89
David Essien <davidessienshare@gmail.com>
2022-06-24 08:46:50 +0100

    update sidebar and widget scripts

--- wp-content/themes/bloggard/lib/sidebars.php
+++ wp-content/themes/bloggard/lib/sidebars.php
@@ -1,38 +1,46 @@
 <?php
 
-function _themename_sidebar_widgets() {
-    register_sidebar( array(
+function _themename_sidebar_widgets()
+{
+    register_sidebar(array(
         'id' => 'primary-sidebar',
-        'name' => esc_html__( 'Primary Sidebar', '_themename' ),
-        'description' => esc_html__( 'This sidebar appears in the blog posts page.', '_themename' ),
+        'name' => esc_html__('Primary Sidebar', '_themename'),
+        'description' => esc_html__('This sidebar appears in the blog posts page.', '_themename'),
         'before_widget' => '<section id="%1$s" class="c-sidebar-widget u-margin-bottom-20 %2$s">',
         'after_widget' => '</section>',
         'before_title' => '<h5>',
         'after_title' => '</h5>'
-    ) );
+    ));
 }
 
-$footer_layout = sanitize_text_field(get_theme_mod('_themename_footer_layout', '3,3,3,3'));
-$footer_layout = preg_replace('/\s+/', '', $footer_layout);
-$columns = explode(',', $footer_layout);
-$footer_bg = _themename_sanitize_footer_bg(get_theme_mod( '_themename_footer_bg', 'dark' ));
-$widget_theme = '';
-if($footer_bg == 'light') {
-    $widget_theme = 'c-footer-widget--dark';
-} else {
-    $widget_theme = 'c-footer-widget--light';
-}
+register_sidebar(array(
+    'id' => 'footer-sidebar-1',
+    'name' => esc_html__('Footer Widgets Column 1', '_themename'),
+    'description' => esc_html__('Footer widgets', '_themename'),
+    'before_widget' => '<section id="%1$s" class="c-footer-widget %2$s">',
+    'after_widget' => '</section>',
+    'before_title' => '<h5>',
+    'after_title' => '</h5>'
+));
 
-foreach ($columns as $i => $column) {
-    register_sidebar( array(
-        'id' => 'footer-sidebar-' . ($i + 1),
-        'name' => sprintf(esc_html__( 'Footer Widgets Column %s', '_themename' ), $i +1),
-        'description' => esc_html__( 'Footer widgets', '_themename' ),
-        'before_widget' => '<section id="%1$s" class="c-footer-widget ' . $widget_theme . ' %2$s">',
-        'after_widget' => '</section>',
-        'before_title' => '<h5>',
-        'after_title' => '</h5>'
-    ) );
-}
+register_sidebar(array(
+    'id' => 'footer-sidebar-2',
+    'name' => esc_html__('Footer Widgets Column 2', '_themename'),
+    'description' => esc_html__('Footer widgets', '_themename'),
+    'before_widget' => '<section id="%1$s" class="c-footer-widget %2$s">',
+    'after_widget' => '</section>',
+    'before_title' => '<h5>',
+    'after_title' => '</h5>'
+));
+
+register_sidebar(array(
+    'id' => 'footer-sidebar-3',
+    'name' => esc_html__('Footer Widgets Column 3', '_themename'),
+    'description' => esc_html__('Footer widgets', '_themename'),
+    'before_widget' => '<section id="%1$s" class="c-footer-widget %2$s">',
+    'after_widget' => '</section>',
+    'before_title' => '<h5>',
+    'after_title' => '</h5>'
+));
 
 add_action('widgets_init', '_themename_sidebar_widgets');
