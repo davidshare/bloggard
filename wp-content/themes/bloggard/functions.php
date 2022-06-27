e0238aeb6005a55fa6735a3154f322d654931b36
David Essien <davidessienshare@gmail.com>
2022-06-27 07:43:57 +0100

    add a helper function to generate copyright date

--- wp-content/themes/bloggard/functions.php
+++ wp-content/themes/bloggard/functions.php
@@ -1,5 +1,6 @@
 <?php
   require_once('lib/customize.php');
+  require_once('lib/helpers.php');
   require_once('lib/enqueue-assets.php');
   require_once('lib/theme-support.php');
   require_once('lib/navigation.php');
