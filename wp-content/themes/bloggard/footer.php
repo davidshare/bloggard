3ac923c0908874c96aa506b184e6527e8b68a55f
David Essien <davidessienshare@gmail.com>
2022-06-27 04:51:41 +0100

    import info template part

--- wp-content/themes/bloggard/footer.php
+++ wp-content/themes/bloggard/footer.php
@@ -1,6 +1,7 @@
   
-  <footer class="footer" role="footer" id="footer">
+  <footer class="c-footer" role="footer" id="footer">
     <?php get_template_part('template-parts/footer/widgets') ?>
+    <?php get_template_part('template-parts/footer/info') ?>
   </footer>
   <?php wp_footer(); ?>
   </body>
